de15a742f39fe04fdcbcbcc1a30593077bd23397
SajedaIsmail <eng.sajedaismail@gmail.com>
2019-09-23 13:26:41 +0300

    Some edits on the Fakedata, is ready now Relates #44

--- server/database/config/FakeData.sql
+++ server/database/config/FakeData.sql
@@ -3,66 +3,66 @@ insert into menu_category (name) values ('Main'),('Drinks'),('Breakfast'),('Sand
 insert into meal (name, category_id, short_desc, description, img, price, ingredients) 
 values ('Checken pizza', 1 , 'Lorem ipsum dolor sit amet, consectetuer adipis.', 
 'Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.',
- 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSI3fydYT9mxhH9Mhmw8j6_UJInxpRu_uZCk8xbAn8SVwCDG3hR', 20.9, ['Nulla', 'Consequat', 'Massa'] ),
+ 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSI3fydYT9mxhH9Mhmw8j6_UJInxpRu_uZCk8xbAn8SVwCDG3hR', 20.9, '{"Nulla", "Consequat", "Massa"}' ),
  
  ('Checken', 1 , 'Lorem ipsum dolor sit amet, consectetuer adipis.', 
 'Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.',
- 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSI3fydYT9mxhH9Mhmw8j6_UJInxpRu_uZCk8xbAn8SVwCDG3hR', 20.9, ['Nulla', 'Consequat', 'Massa'] ),
+ 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSI3fydYT9mxhH9Mhmw8j6_UJInxpRu_uZCk8xbAn8SVwCDG3hR', 20.9, '{"Nulla", "Consequat", "Massa"}' ),
 
 ('Checken With Cheese', 1 , 'Lorem ipsum dolor sit amet, consectetuer adipis.', 
 'Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.',
- 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSI3fydYT9mxhH9Mhmw8j6_UJInxpRu_uZCk8xbAn8SVwCDG3hR', 20.9, ['Nulla', 'Consequat', 'Massa'] ),
+ 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSI3fydYT9mxhH9Mhmw8j6_UJInxpRu_uZCk8xbAn8SVwCDG3hR', 20.9, '{"Nulla", "Consequat", "Massa"}' ),
  
  ('Juice', 2 , 'Lorem ipsum dolor sit amet, consectetuer adipis.', 
 'Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.',
- 'https://www.foodbusinessnews.net/ext/resources/2019/5/FruitJuices_Lead.jpg?1558529580', 11.9, ['Nulla', 'Consequat', 'Massa'] ),
+ 'https://www.foodbusinessnews.net/ext/resources/2019/5/FruitJuices_Lead.jpg?1558529580', 11.9, '{"Nulla", "Consequat", "Massa"}' ),
 
  ('cola', 2 , 'Lorem ipsum dolor sit amet, consectetuer adipis.', 
 'Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.',
- 'https://www.foodbusinessnews.net/ext/resources/2019/5/FruitJuices_Lead.jpg?1558529580', 11.9, ['Nulla', 'Consequat', 'Massa'] ),
+ 'https://www.foodbusinessnews.net/ext/resources/2019/5/FruitJuices_Lead.jpg?1558529580', 11.9, '{"Nulla", "Consequat", "Massa"}' ),
 
  ('Tea', 2 , 'Lorem ipsum dolor sit amet, consectetuer adipis.', 
 'Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.',
- 'https://www.foodbusinessnews.net/ext/resources/2019/5/FruitJuices_Lead.jpg?1558529580', 11.9, ['Nulla', 'Consequat', 'Massa'] ),
+ 'https://www.foodbusinessnews.net/ext/resources/2019/5/FruitJuices_Lead.jpg?1558529580', 11.9, '{"Nulla", "Consequat", "Massa"}' ),
 
  ('Falafel', 3 , 'Lorem ipsum dolor sit amet, consectetuer adipis.', 
 'Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.',
- 'https://modo3.com/thumbs/fit630x300/100367/1466038724/%D8%B7%D8%B1%D9%8A%D9%82%D8%A9_%D8%A7%D9%84%D9%81%D9%84%D8%A7%D9%81%D9%84_%D8%A7%D9%84%D8%B3%D9%88%D8%B1%D9%8A%D8%A9.jpg', 4.9, ['Nulla', 'Consequat', 'Massa'] ),
+ 'https://modo3.com/thumbs/fit630x300/100367/1466038724/%D8%B7%D8%B1%D9%8A%D9%82%D8%A9_%D8%A7%D9%84%D9%81%D9%84%D8%A7%D9%81%D9%84_%D8%A7%D9%84%D8%B3%D9%88%D8%B1%D9%8A%D8%A9.jpg', 4.9, '{"Nulla", "Consequat", "Massa"}' ),
 
 ('Pancake', 3 , 'Lorem ipsum dolor sit amet, consectetuer adipis.', 
 'Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.',
- 'https://d3awvtnmmsvyot.cloudfront.net/api/file/6fTZjAw9Tjqf4XrddmRQ', 4.9, ['Nulla', 'Consequat', 'Massa'] ),
+ 'https://d3awvtnmmsvyot.cloudfront.net/api/file/6fTZjAw9Tjqf4XrddmRQ', 4.9, '{"Nulla", "Consequat", "Massa"}' ),
 
 ('Omlet', 3 , 'Lorem ipsum dolor sit amet, consectetuer adipis.', 
 'Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.',
- 'https://st.depositphotos.com/1030624/2429/i/450/depositphotos_24294999-stock-photo-omelette-with-salad.jpg', 4.9, ['Nulla', 'Consequat', 'Massa'] ),
+ 'https://st.depositphotos.com/1030624/2429/i/450/depositphotos_24294999-stock-photo-omelette-with-salad.jpg', 4.9, '{"Nulla", "Consequat", "Massa"}' ),
 
 ('Burger', 4 , 'Lorem ipsum dolor sit amet, consectetuer adipis.', 
 'Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.',
- 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQMG2eoSp8Ju6h6kpqu7PvIdEs2Hj_ystsmY0Rf_ij8t7Q3IRZyw', 4.9, ['Nulla', 'Consequat', 'Massa'] ),
+ 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQMG2eoSp8Ju6h6kpqu7PvIdEs2Hj_ystsmY0Rf_ij8t7Q3IRZyw', 4.9, '{"Nulla", "Consequat", "Massa"}' ),
 
 ('Mosakhan', 4 , 'Lorem ipsum dolor sit amet, consectetuer adipis.', 
 'Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.',
- 'https://assets.nn.ps/CACHE/images/uploads/weblog/2019/02/12/37563360_256897665157434_8887038800081977344_n/78b3af40a8a8441e37ea224cf14d3c00.jpg', 4.9, ['Nulla', 'Consequat', 'Massa'] ),
+ 'https://assets.nn.ps/CACHE/images/uploads/weblog/2019/02/12/37563360_256897665157434_8887038800081977344_n/78b3af40a8a8441e37ea224cf14d3c00.jpg', 4.9, '{"Nulla", "Consequat", "Massa"}' ),
 
 ('Shawerma', 4 , 'Lorem ipsum dolor sit amet, consectetuer adipis.', 
 'Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.',
- 'https://mybayutcdn.bayut.com/mybayut/wp-content/uploads/shawerma-in-dubai-.jpg', 4.9, ['Nulla', 'Consequat', 'Massa'] ),
+ 'https://mybayutcdn.bayut.com/mybayut/wp-content/uploads/shawerma-in-dubai-.jpg', 4.9, '{"Nulla", "Consequat", "Massa"}' ),
 
 ('Nabulsia', 5 , 'Lorem ipsum dolor sit amet, consectetuer adipis.', 
 'Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.',
- 'https://lh3.googleusercontent.com/Tf7w4wZvEau-Ain-A99DTRjYTDGMSUaiQn6_xyQ3_A1IEg7N-s8j7FHRTC9QV14IsRNLK5pM=w1080-h608-p-no-v0', 4.9, ['Nulla', 'Consequat', 'Massa'] ),
+ 'https://lh3.googleusercontent.com/Tf7w4wZvEau-Ain-A99DTRjYTDGMSUaiQn6_xyQ3_A1IEg7N-s8j7FHRTC9QV14IsRNLK5pM=w1080-h608-p-no-v0', 4.9, '{"Nulla", "Consequat", "Massa"}' ),
 
 ('Cheese Cake', 5 , 'Lorem ipsum dolor sit amet, consectetuer adipis.', 
 'Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.',
- 'https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto:eco/https%3A%2F%2Fstorage.googleapis.com%2Fgen-atmedia%2F3%2F2019%2F03%2Fe689424fc831585626336b141c268f0253ea2dd2.jpeg', 4.9, ['Nulla', 'Consequat', 'Massa'] ),
+ 'https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto:eco/https%3A%2F%2Fstorage.googleapis.com%2Fgen-atmedia%2F3%2F2019%2F03%2Fe689424fc831585626336b141c268f0253ea2dd2.jpeg', 4.9, '{"Nulla", "Consequat", "Massa"}' ),
 
 ('Oreo Cake', 5 , 'Lorem ipsum dolor sit amet, consectetuer adipis.', 
 'Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.',
- 'https://www.wishacupcake.com/wp-content/uploads/2017/08/Oreo-Cake.jpg', 4.9, ['Nulla', 'Consequat', 'Massa'] );
+ 'https://www.wishacupcake.com/wp-content/uploads/2017/08/Oreo-Cake.jpg', 4.9, '{"Nulla", "Consequat", "Massa"}');
 
-insert into user_order (create_at,  total_price, table_no) values ('Mon Sep 23 2019 10:58:57 GMT+0300 (Israel Daylight Time)', 65.6, 1 ), ('Mon Sep 23 2019 11:58:57 GMT+0300 (Israel Daylight Time)', 50, 2 ), ('Mon Sep 24 2019 10:58:57 GMT+0300 (Israel Daylight Time)', 200, 3 );
-insert into order_meal (order_id, meal_id, vegetables, fruits, nuts, sauce, salt, spices, sugar, amount, price) values (1, 1, [tomato, onion],null,null, 1, 2, 3, null, 2, 41.8) , 
-(1, 4, null, [panana, strawberry], null, null,null,null, 2, 2, 23.8);
+insert into user_order (create_at,  total_price, table_no) values ('2016-06-22 19:10:25-07', 65.6, 1 ), ('2016-06-22 19:10:25-07', 50, 2 ), ('2016-06-22 19:10:25-07', 200, 3 );
+insert into order_meal (order_id, meal_id, vegetables, fruits, nuts, sauce, salt, spices, sugar, amount, price) values (1, 1, '{"tomato", "onion"}',null,null, 1, 2, 3, null, 2, 41.8) , 
+(1, 4, null, '{"panana", "strawberry"}', null, null,null,null, 2, 2, 23.8);
 
 insert into feedback (order_id, email, feedback) values (1, 'ssss@gmail.com' , 'The food was very cold and the salt was more than I requested -_- ');
