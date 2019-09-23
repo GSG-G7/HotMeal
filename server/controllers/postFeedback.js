02df3cef9d8889c91e604f3f813cee39fab55df5
alaa-yasin <alaayasin.ps@gmail.com>
2019-09-23 12:44:18 +0300

    commit the files Relates #26

--- server/controllers/postFeedback.js
+++ server/controllers/postFeedback.js
@@ -1,15 +1,15 @@
-const postFeedback = require('../database/config/connection');
-const feedbackSchema = require('../validation/feedback');
+// const postFeedback = require('../database/config/connection');
+// const feedbackSchema = require('../validation/feedback');
 
-const PostFeedback = (req, res, next) => {
-  feedbackSchema.validate(req.body, (err) => {
-    if (err) {
-      res.send('fill all the fields');
-    } else {
-      postFeedback(req.body)
-        .then(res.send('Your feedback has sent!'))
-        .catch((error) => next(error));
-    }
-  });
-};
-module.exports = PostFeedback;
+// const PostFeedback = (req, res, next) => {
+//   feedbackSchema.validate(req.body, (err) => {
+//     if (err) {
+//       res.send('fill all the fields');
+//     } else {
+//       postFeedback(req.body)
+//         .then(res.send('Your feedback has sent!'))
+//         .catch((error) => next(error));
+//     }
+//   });
+// };
+// module.exports = PostFeedback;
--- server/database/queries/postFeedback.js
+++ server/database/queries/postFeedback.js
@@ -1,11 +1,11 @@
-const connection = require('../config/connection');
+// const connection = require('../config/connection');
 
-const postFeedback = (data) => {
-  const { orderID, email, feedback } = data;
-  const sql = {
-    text: 'INSERT INTO feedback (orderID, email, feedback) values ($1, $2, $3)',
-    values: [orderID, email, feedback],
-  };
-  return connection.query(sql);
-};
-module.exports = postFeedback;
+// const postFeedback = (data) => {
+//   const { orderID, email, feedback } = data;
+//   const sql = {
+//     text: 'INSERT INTO feedback (orderID, email, feedback) values ($1, $2, $3)',
+//     values: [orderID, email, feedback],
+//   };
+//   return connection.query(sql);
+// };
+// module.exports = postFeedback;
