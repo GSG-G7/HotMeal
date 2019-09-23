916968331a01003fee85310abcf27c8fa4d458b5
alaa-yasin <alaayasin.ps@gmail.com>
2019-09-23 12:53:08 +0300

    add test to post feedback route Relates #26

--- test/postFeedbackTest.js
+++ test/postFeedbackTest.js
@@ -1,15 +1,15 @@
-// const tape = require('tape');
+const tape = require('tape');
 
-// const postFeedback = require('../server/database/queries/postFeedback');
+const build = require('../server/database/config/build');
+const postFeedback = require('../server/database/queries/postFeedback');
 
-// tape('testing post feedback route', (t) => {
-//   const expected = 'alaayasin.ps@gmail.com';
-//   const userFeedback = { email: 'alaayasin.ps@gmail.com', feedback: 'The meal was delicious' };
-//   build()
-//     .then(() => postFeedback(userFeedback))
-//     .then((res) => t.deepEqual(res.email, expected, 'must be the same'))
-//     .then(() => t.end())
-//     .catch();
-// });
+tape('testing post feedback route', (t) => {
+  const expected = 'alaayasin.ps@gmail.com';
+  build()
+    .then(() => postFeedback())
+    .then((res) => t.deepEqual(res.email, expected, 'must be the same'))
+    .then(() => t.end())
+    .catch();
+});
 
-// tape.onFinish(() => process.exit(0));
+tape.onFinish(() => process.exit(0));
