327db89d0928deb5bf144fb4a9c599bbe3446f9c
alaa-yasin <alaayasin.ps@gmail.com>
2019-09-24 10:35:51 +0300

    add more scripts Relates #26

---
+++ server/controllers/feedback.test.js
@@ -0,0 +1,20 @@
+const postFeedback = require('../database/queries/postFeedback');
+const feedbackSchema = require('../validation/feedback');
+
+module.exports = (req, res, next) => {
+  feedbackSchema
+    .validateAsync(req.body)
+    .then((data) => postFeedback(data))
+    .then(() => {
+      res.status(200).send({ statusCode: 200, message: 'Your feedback has sent!' });
+    })
+    .catch((err) => {
+      if (err.details) {
+        if (err.details[0].message) {
+          res.status(400).send({ statusCode: 400, message: err.details[0].message });
+        }
+      } else {
+        next(err);
+      }
+    });
+};
