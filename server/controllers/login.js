7d492fb7497336866a079560f0226222c587de5e
MohammadAlhalaq <m-a-s-h@outlook.com>
2019-09-24 12:02:24 +0300

    convert data and message to error in the error schema Relates #30

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -5,11 +5,11 @@ module.exports = (req, res, next) => {
   loginSchema.validateAsync(req.body)
     .then(() => loginQuery(req.body))
     .then((tables) => (tables.rows[0] ? res.status(200).send({ statusCode: 200 })
-      : res.status(401).send({ statusCode: 401, data: 'login false' })))
+      : res.status(401).send({ statusCode: 401, error: 'login false' })))
     .catch((err) => {
       if (err.details) {
         if (err.details[0].message) {
-          res.status(400).send({ statusCode: 400, message: err.details[0].message });
+          res.status(400).send({ statusCode: 400, error: err.details[0].message });
         }
       } else {
         next(err);
