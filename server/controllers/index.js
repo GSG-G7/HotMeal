91783b6f4ed3cce30351061bbf40f27340e88459
alaa-yasin <alaayasin.ps@gmail.com>
2019-09-24 10:38:11 +0300

    edit post feedback route Relates #26

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,16 +1,8 @@
 const express = require('express');
-const postFeedback = require('./postFeedback');
+const postFeedback = require('./feedback.test');
 
 const router = express.Router();
 
-router.post('/postfeedback', postFeedback);
-
-router.use((req, res) => {
-  res.status(400).send('Client Error');
-});
-// eslint-disable-next-line no-unused-vars
-router.use((err, req, res, next) => {
-  res.status(500).send('Internal server error');
-});
+router.post('/post-feedback', postFeedback);
 
 module.exports = router;
