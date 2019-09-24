27c421146ab9237c73ee34d34a69138a85e6289d
rana <ranasobeid95@gmail.com>
2019-09-24 15:49:16 +0300

    add validation for req.body

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -1,19 +1,22 @@
-/* eslint-disable no-console */
 const addOrder = require('../database/queries/order');
 const addMeal = require('../database/queries/orderMeal');
+const orderSchema = require('../validation/order');
 
 module.exports = (req, res, next) => {
   const {
     createdAt, totalPrice, tableNumber, meals,
   } = req.body;
-
-  addOrder(createdAt, totalPrice, tableNumber)
-    .then((order) => {
-      const orderId = order.rows[0].id;
-      return Promise.all(meals.map((meal) => addMeal(orderId, meal)));
-    }).then(() => {
-      res.send({ statusCode: 200 });
-    })
+  orderSchema
+    .validateAsync(req.body)
+    .then(
+      addOrder(createdAt, totalPrice, tableNumber)
+        .then((order) => {
+          const orderId = order.rows[0].id;
+          return Promise.all(meals.map((meal) => addMeal(orderId, meal)));
+        }).then(() => {
+          res.send({ statusCode: 200 });
+        }),
+    )
     .catch((error) => {
       next(error);
     });
