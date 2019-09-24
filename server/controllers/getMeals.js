8bd6467df7d62ad76b8458cfeef8b2a1c2eca10c
SajedaIsmail <eng.sajedaismail@gmail.com>
2019-09-24 13:53:34 +0300

    Give a default value to the category query Relates #27

--- server/controllers/getMeals.js
+++ server/controllers/getMeals.js
@@ -1,15 +1,15 @@
 const getMealsByCat = require('../database/queries/getMealsByCat');
 
 const getMeals = (req, res, next) => {
-  let { category } = req.query;
+  let { category = 'main' } = req.query;
   if (category) category = category.toLowerCase();
   getMealsByCat(category)
     .then((result) => (result.rows.length
-        ? res.status(200).send({ code: 200, data: result.rows })
-        : res.status(400).send({
-            code: 400,
-            error: 'Bad Request: there is no such category'
-          })),)
+      ? res.status(200).send({ code: 200, data: result.rows })
+      : res.status(400).send({
+        code: 400,
+        error: 'Bad Request: there is no such category'
+      })),)
     .catch((err) => next(err));
 };
 
