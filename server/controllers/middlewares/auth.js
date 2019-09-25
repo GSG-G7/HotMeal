a90c134373f88fa944c7624c7600bb816f0276d4
MohammadAlhalaq <m-a-s-h@outlook.com>
2019-09-25 11:30:19 +0300

    eddit query to select table number and in  auth edit the object schema key data be error:unauthorized and assign object with tablenumber key in req.user Relates #30

--- server/controllers/middlewares/auth.js
+++ server/controllers/middlewares/auth.js
@@ -3,9 +3,9 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   jwt.verify(req.cookies.hotmeal_token, process.env.PRIVATEKEY, (err, token) => {
     if (err) {
-      res.status(401).send({ statusCode: 401, data: { logedIn: 0 } }); // hmmmm logedIn 0 == false
+      res.status(401).send({ statusCode: 401, error: 'Unauthorized' });
     } else {
-      req.user = token.tableNumber;
+      req.user = { tableNumber: token.tableNumber };
       next();
     }
   });
--- server/database/queries/login.js
+++ server/database/queries/login.js
@@ -3,6 +3,6 @@ const connection = require('../config/connection');
 module.exports = (data) => {
   const { secret, tableNumber } = data;
   return connection.query(
-    'SELECT * FROM table_info WHERE $1 = number and $2 = secret;', [tableNumber, secret],
+    'SELECT number FROM table_info WHERE $1 = number And $2 = secret;', [tableNumber, secret],
   );
 };
