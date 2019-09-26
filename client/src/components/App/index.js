047da679163aed0cc309af1d66ea44856d67566b
alaa-yasin <alaayasin.ps@gmail.com>
2019-09-26 09:30:03 +0300

    romove mu updates Relates #35

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,11 +1,4 @@
 import React from 'react';
-import Button from '../Utils/Button/index';
 import './style.css';
 
-export default () => {
-  return (
-    <div>
-      <Button />
-    </div>
-  );
-};
+export default () => <h1>Hello World</h1>;
