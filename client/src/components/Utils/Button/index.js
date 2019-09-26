8b5117b25cbab363450a352a34ea4d88f2d28907
alaa-yasin <alaayasin.ps@gmail.com>
2019-09-26 09:31:24 +0300

    remove text from the button and make it a property Relates #35

--- client/src/components/Utils/Button/index.js
+++ client/src/components/Utils/Button/index.js
@@ -3,14 +3,15 @@ import PropTypes from 'prop-types';
 
 import './style.css';
 
-export default function Button({ className }) {
+export default function Button({ className, text }) {
   return (
     <button type="button" className={`button ${className}`}>
-      Enter
+      {text}
     </button>
   );
 }
 
 Button.propTypes = {
   className: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
 };
