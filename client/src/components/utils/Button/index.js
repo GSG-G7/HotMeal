2efc3ca6751eb703cfbcb92d46a6951f87320ec4
alaa-yasin <alaayasin.ps@gmail.com>
2019-09-29 16:56:07 +0300

    solve conflict in Botton component Relates #57

--- client/src/components/utils/Button/index.js
+++ client/src/components/utils/Button/index.js
@@ -3,14 +3,40 @@ import PropTypes from 'prop-types';
 
 import './style.css';
 
-export default function Button({ className }) {
-  return (
-    <button type="submit" className={`button ${className}`}>
-      Enter
+const Button = ({
+  type = 'button',
+  className = '',
+  onClick = undefined,
+  children,
+}) => {
+  return type === 'submit' ? (
+    <button
+      type="submit"
+      className={`common-button ${className}`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  ) : (
+    <button
+      type="button"
+      className={`common-button ${className}`}
+      onClick={onClick}
+    >
+      {children}
     </button>
   );
-}
+};
+Button.defaultProps = {
+  onClick: undefined,
+  type: 'button',
+};
 
 Button.propTypes = {
+  type: PropTypes.string,
   className: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  children: PropTypes.string.isRequired,
 };
+
+export default Button;
