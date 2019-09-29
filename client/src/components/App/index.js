e49536dbc39875d241e28483cc3bd60f7752cdcc
rana <ranasobeid95@gmail.com>
2019-09-29 09:46:35 +0300

    Create App component which div contain MealList and pass fake data as props make validation for props

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,9 +1,91 @@
 import React from 'react';
-import MealPage from '../mealPage';
+import PropTypes from 'prop-types';
+
+import MealList from '../mealPage';
+import srcImg from '../../assets/screen-0.jpg';
 import './style.css';
 
-export default () => (
+const App = ({ category }) => (
   <div className="app">
-    <MealPage />
+    <header className="header">
+      <nav className="nav">
+        <ul className="header__nav">
+          <li>
+            <i className="fas fa-arrow-left"></i>
+          </li>
+          <li className="category">{category}</li>
+          <li>
+            <i className="fas fa-chevron-circle-down"></i>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <MealList
+      data={[
+        {
+          img: srcImg,
+          name: 'Shrimp Pasta',
+          shortDesc:
+            'Lorem ipsum dolor site, amet consectetur adipisicing elit',
+          price: '25 NIS',
+        },
+        {
+          img: srcImg,
+          name: 'Shrimp Pasta',
+          shortDesc:
+            'Lorem ipsum dolor site, amet consectetur adipisicing elit',
+          price: '25 NIS',
+        },
+        {
+          img: srcImg,
+          name: 'Shrimp Pasta',
+          shortDesc:
+            'Lorem ipsum dolor site, amet consectetur adipisicing elit',
+          price: '25 NIS',
+        },
+        {
+          img: srcImg,
+          name: 'Shrimp Pasta',
+          shortDesc:
+            'Lorem ipsum dolor site, amet consectetur adipisicing elit',
+          price: '25 NIS',
+        },
+        {
+          img: srcImg,
+          name: 'Shrimp Pasta',
+          shortDesc:
+            'Lorem ipsum dolor site, amet consectetur adipisicing elit',
+          price: '25 NIS',
+        },
+        {
+          img: srcImg,
+          name: 'Shrimp Pasta',
+          shortDesc:
+            'Lorem ipsum dolor site, amet consectetur adipisicing elit',
+          price: '25 NIS',
+        },
+        {
+          img: srcImg,
+          name: 'Shrimp Pasta',
+          shortDesc:
+            'Lorem ipsum dolor site, amet consectetur adipisicing elit',
+          price: '25 NIS',
+        },
+        {
+          img: srcImg,
+          name: 'Shrimp Pasta',
+          shortDesc:
+            'Lorem ipsum dolor site, amet consectetur adipisicing elit',
+          price: '25 NIS',
+        },
+      ]}
+    />
+    <span className="float">
+      <i className="fa fa-plus my-float"></i>
+    </span>
   </div>
 );
+App.propTypes = {
+  category: PropTypes.string.isRequired,
+};
+export default App;
