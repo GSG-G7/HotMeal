2a7b352c939de8333e2bc0d809489597823771c1
SajedaIsmail <eng.sajedaismail@gmail.com>
2019-09-29 10:57:54 +0300

    A prop for defining if the pop up has one or tow buttons is added the Roboto font family is used the background color is black and lighter Relates #36

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './style.css';
-import PopUp from '../utils/PopUp/index';
 
 export default () => (
   <div>
-    <PopUp message="The waiter is coming" />
     <h1>Hello World</h1>
   </div>
 );
--- client/src/components/utils/PopUp/index.js
+++ client/src/components/utils/PopUp/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import './style.css';
-import img from '../../../assets/images/popupImg.png';
+// import img from '../../../assets/images/popupImg.png';
+// import imgSVG from '../../../assets/images/popupimg.svg';
 // import Button from '../../utils/Button/index';
 
-function PopUp({ message }) {
+function PopUp({ message, is2btnNeeded }) {
+  if (is2btnNeeded) {
+    return (
+      <div className="popup-background">
+        <div className="popup">
+          <div className="popup-img-container" />
+          <p className="popup-text"> {message} </p>
+          <div className="popup-btncontainer">
+            <button type="submit" className="popup-2btns">
+              confirm
+            </button>
+            <button type="submit" className="popup-2btns">
+              cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="popup-background">
       <div className="popup">
-        <img src={img} alt="food" className="popup-img" />
+        <div className="popup-img-container" />
         <p className="popup-text"> {message} </p>
         <button type="submit" className="popup-button">
           submit
@@ -20,5 +40,6 @@ function PopUp({ message }) {
 
 PopUp.propTypes = {
   message: propTypes.string.isRequired,
+  is2btnNeeded: propTypes.bool.isRequired,
 };
 export default PopUp;
