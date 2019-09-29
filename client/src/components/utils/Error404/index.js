9721d732148be4546846da40e3680ee5477fed3f
alaa-yasin <alaayasin.ps@gmail.com>
2019-09-29 19:26:49 +0300

    wrap the button with a link to the home page Relates #57

--- client/src/components/utils/Error404/index.js
+++ client/src/components/utils/Error404/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import error404 from '../../../assets/images/error.jpg';
 import Button from '../Button/index';
 import './style.css';
@@ -7,9 +8,11 @@ const Error404 = () => {
   return (
     <div>
       <img className="error404__image" src={error404} alt="error-img" />
-      <Button type="button" className="error404__button">
-        Go Back
-      </Button>
+      <Link to="/">
+        <Button type="button" className="error404__button">
+          Go Back
+        </Button>
+      </Link>
     </div>
   );
 };
