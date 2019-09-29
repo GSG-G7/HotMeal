22cb1a1038eb957170c3515b8f4046ed03ee80ed
alaa-yasin <alaayasin.ps@gmail.com>
2019-09-29 18:38:22 +0300

    add button to Error404 Component Relates #57

--- client/src/components/utils/Error404/index.js
+++ client/src/components/utils/Error404/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import error404 from '../../../assets/images/error.jpg';
+import Button from '../Button/index';
 import './style.css';
 
 const Error404 = () => {
-  return <img className="error404__image" src={error404} alt="error-img" />;
+  return (
+    <div>
+      <img className="error404__image" src={error404} alt="error-img" />
+      <Button type="button" className="error404__button">
+        Go Back
+      </Button>
+    </div>
+  );
 };
 export default Error404;
