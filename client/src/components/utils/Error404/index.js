245fbaa36d34c3f59023b9b3539a676ec2f8d786
alaa-yasin <alaayasin.ps@gmail.com>
2019-09-29 15:21:36 +0300

    edit the file name Relates #57

---
+++ client/src/components/utils/Error404/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import error404 from '../../../assets/images/error.jpg';
+import './style.css';
+
+const Error404 = () => {
+  return <img className="error404__image" src={error404} alt="error-img" />;
+};
+export default Error404;
