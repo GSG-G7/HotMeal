b91dad4bc686668bfb301df788c3f9d419f2d01a
alaa-yasin <alaayasin.ps@gmail.com>
2019-10-01 17:22:02 +0300

    pass the table number to the parent component Relates #38

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,16 +1,33 @@
+/* eslint-disable react/no-unused-state */
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from '../utils/Login/index';
 import Error404 from '../utils/Error404/index';
 import './style.css';
 
-export default () => {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/login" component={Login} />
-        <Route exact path="*" component={Error404} />
-      </Switch>
-    </Router>
-  );
-};
+export default class App extends React.Component {
+  state = {
+    tableNumber: null,
+  };
+
+  updateTableNumber = e => {
+    this.setState({ tableNumber: e });
+  };
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path="/login"
+            component={props => (
+              <Login updateTableNumber={this.updateTableNumber} {...props} />
+            )}
+          />
+          <Route exact path="*" component={Error404} />
+        </Switch>
+      </Router>
+    );
+  }
+}
