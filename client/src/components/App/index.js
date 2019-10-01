8606dc2d3c3c3d6864554a36d13a5f01188241ee
alaa-yasin <alaayasin.ps@gmail.com>
2019-10-01 12:15:01 +0300

    add login route Relates #38

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from '../utils/Login/index';
 import Error404 from '../utils/Error404/index';
 import './style.css';
 
@@ -7,6 +8,7 @@ export default () => {
   return (
     <Router>
       <Switch>
+        <Route exact path="/login" component={Login} />
         <Route exact path="*" component={Error404} />
       </Switch>
     </Router>
