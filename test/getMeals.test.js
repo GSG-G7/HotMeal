4ab8b39d291892cd53abe8aa4bef71e64a80a147
asem1789 <as.1789@hotmail.com>
2019-10-08 10:56:54 +0300

    separate token in file, which it fix test Relates #3

--- test/getMeals.test.js
+++ test/getMeals.test.js
@@ -1,27 +1,19 @@
 const supertest = require('supertest');
 const test = require('tape');
 const app = require('../server/app');
-
+const { token } = require('./cookie.test');
 
 test('Testing for getMeals route', (t) => {
   supertest(app)
     .get('/api/v1/meals')
-    .set('Cookie', ['hotmeal_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0YWJsZU51bWJlciI6MSwiaWF0IjoxNTY5Mzk2ODEyfQ.-Rb_346P1wnl0VHCmmf7zGXaBrfwMlxktFNUEt4l_kk'])
+    .set('Cookie', [`hotmeal_token=${token}`])
     .query({ category: 'main' })
     .expect(200)
     .expect('Content-Type', /json/)
     .end((err, res) => {
       t.error(err);
-      t.deepEqual(
-        Object.keys(res.body),
-        ['statusCode', 'data'],
-        'The keys must be the same',
-      );
-      t.equal(
-        res.body.statusCode,
-        200,
-        'The code value must be 200 in the response body',
-      );
+      t.deepEqual(Object.keys(res.body), ['statusCode', 'data'], 'The keys must be the same');
+      t.equal(res.body.statusCode, 200, 'The code value must be 200 in the response body');
       res.body.data.forEach((ele) => t.equal(ele.category_id, 1, 'All category ids must be 1'));
       res.body.data.forEach((ele) => t.equal(ele.category, 'main', 'All category names must be main'));
       t.end();
@@ -31,7 +23,7 @@ test('Testing for getMeals route', (t) => {
 test('Testing for getMeals route', (t) => {
   supertest(app)
     .get('/api/v1/meals')
-    .set('Cookie', ['hotmeal_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0YWJsZU51bWJlciI6MSwiaWF0IjoxNTY5Mzk2ODEyfQ.-Rb_346P1wnl0VHCmmf7zGXaBrfwMlxktFNUEt4l_kk'])
+    .set('Cookie', [`hotmeal_token=${token}`])
     .query({ category: 'blabla' })
     .expect(400)
     .expect('Content-Type', /json/)
